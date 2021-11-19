17feb393cf5fc96f057148ea76f2338fab670505
mateo <mateo-6543@hotmail.com>
2021-11-19 13:13:27 -0500

    La base

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-ValidityState
-asfads 
+const express = require('express')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const db = require('./dao/models')
+
+const PORT = 5000
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended : true
+}))
+app.use(express.static('assets'))
+app.set('view engine', 'ejs') 
+app.use(session({
+    secret : "daleu",
+    resave : false,
+    saveUninitialized : false
+}))
