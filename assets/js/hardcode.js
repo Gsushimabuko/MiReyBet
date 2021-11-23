ddfb652e16274bfaf2ea3605736d9aa4cbef7d1e
Gabriel Shimabuko <20171470@aloe.ulima.edu.pe>
2021-11-22 22:19:48 -0500

    Apuestas funcionando, (faltan combinadas y  despintado)

--- assets/js/hardcode.js
+++ assets/js/hardcode.js
@@ -2,97 +2,182 @@
 
 let cartilla = false
 let total = 0
+let intentos = 0
+let vacio = false
 
 const inputTotal = document.getElementById("total")
+const longitud = document.getElementById("tabla").childElementCount
+const inicio = 110
+
+
+
 
 const obtenerFactores = () =>{
 
+    for (let i = inicio; i < inicio+longitud; i++) {
 
-    const butlocal = document.getElementById("butlocal110")
-    const butvisita = document.getElementById("butvisita110")
-    const butempate = document.getElementById("butempate110")
+        const butlocal = document.getElementById("butlocal" + i)
+        const butvisita = document.getElementById("butvisita" + i)
+        const butempate = document.getElementById("butempate" + i)
+        
+
+        butlocal.onclick = () =>{
+            
+            const factorlocal = butlocal.innerText
+            crearCartilla(factorlocal,butlocal.id,i)
+            
+            
+        }
+        
+        butempate.onclick = () =>{
+            
+            const factorempate = butempate.innerText
+            crearCartilla(factorempate,butempate.id,i)
+            
+        }
+        
+        butvisita.onclick = () =>{
+            
+            const factorvisita = butvisita.innerText
+            crearCartilla(factorvisita,butvisita.id,i)
+            
+            
+            
+
+        }
+        
+    }
     
-    butlocal.onclick = () =>{
-        
-        const factorlocal = butlocal.innerText
-        crearCartilla(factorlocal,butlocal.id)
-    }
-
-    butempate.onclick = () =>{
-        
-        const factorempate = butempate.innerText
-        crearCartilla(factorempate,butempate.id)
-    }
-
-    butvisita.onclick = () =>{
-        
-        const factorvisita = butvisita.innerText
-        crearCartilla(factorvisita,butvisita.id)
-    }
     
     
 } 
 
-const crearCartilla = (factor,idBoton) =>{
+
+
+const crearCartilla = (factor,idBoton,idLinea) =>{
     
     //ACCEDIENDO AL DIV VACÍO DE LA CARTILLA
     const divCartilla = document.getElementById("cartilla")
     const divMultiplicador = document.getElementById("multiplicador")
 
+
     if(cartilla == false) {
 
-        //ACCEDIENDO AL PARTIDO
-      
-        const trApuesta = document.getElementById(idBoton).parentNode.parentNode.parentNode
+        
+
+        //nombre del equipo Local
+        const nombreLocal = document.getElementById(idLinea).children[2].children[0].innerText
         
         
-        console.log("TR APUESTA: ", trApuesta[1])
+        //nombre visita
+        const nombreVisita = document.getElementById(idLinea).children[4].children[0].innerText
+        
 
+        console.log("PARTIDO: ", nombreLocal, " vs ", nombreVisita)
 
+        //Texto partido
+        const textoPartido = idLinea + " " + nombreLocal + " vs " + nombreVisita + "\n"
+        const textoPartidoHTML = document.createElement("text")
+        textoPartidoHTML.innerText = textoPartido
+
+        //CODIGO PARTIDA
+        const codigopartida = document.createElement("input")
+        codigopartida.setAttribute("id", "codigoPartidaFinal")
+        codigopartida.setAttribute("type","hidden")
+        codigopartida.value = idLinea
+        
     
         //ACCEDIENDO AL NOMBRE DEL EQUIPO DEL FACTOR
         const nombreEquipo = document.getElementById(idBoton).parentNode.previousElementSibling.innerText
-        const nombreHTML = document.createTextNode(nombreEquipo)
+        const nombreHTML = document.createElement("div")
+        nombreHTML.setAttribute("id","nombreseleccion")
+        nombreHTML.innerText = nombreEquipo
         //CREANDO EL TEXTO CON EL NUMERO  DEL FACTOR
         const factorApuesta = document.createTextNode(factor)
+        
     
         
         console.log(nombreEquipo, factorApuesta)
     
         //AGREGANDO EL FACTOR
+        divCartilla.appendChild(codigopartida)
+        divCartilla.appendChild(textoPartidoHTML)
         divCartilla.appendChild(nombreHTML)
         divMultiplicador.appendChild(factorApuesta)
-        //Borrar selección
-        const botonBorrar = document.getElementById("borrar")
-        botonBorrar.onclick = borrarSeleccion
+
+        //Agregar Botón cerrar
+        agregarBotonCerrar()
 
         cartilla = true
-
-
         actualizarGanancia()
+        confirmarApuesta()
+
+
 
     }else{
 
         divCartilla.innerHTML = ""
         divMultiplicador.innerHTML = ""
+
+       
+        
+        const divCerrar = document.getElementById("cerrar")   
+        divCerrar.removeChild(divCerrar.childNodes[0,1]);
+            
+
+
+        intentos = 0
         cartilla = false
 
-        
+       
     }
 
 
 }
 
-const borrarSeleccion = () => {
-
-    const divSeleccion = document.getElementById("datosseleccion")
-    const nombreClub = divSeleccion.firstElementChild
-    const factorPartido = nombreClub.nextElementSibling
-
-    nombreClub.innerText = ""
-    factorPartido.innerText = ""
+const agregarBotonCerrar = () => {
 
 
+        if( intentos == 0){
+            //creando boton
+            console.log("Wntrando")
+            const botonCerrar = document.createElement("button")
+            botonCerrar.setAttribute("id", "butcerrar")
+            botonCerrar.innerText = "Borrar"
+            
+            
+            //buscar div
+            const divCerrar = document.getElementById("cerrar")
+            //agregar boton al div
+            divCerrar.appendChild(botonCerrar)
+            intentos++
+
+            }
+
+        const botonCerrar = document.getElementById("butcerrar")
+
+            botonCerrar.onclick = () =>{
+    
+                const divSeleccion = document.getElementById("datosseleccion")
+                const nombreClub = divSeleccion.firstElementChild
+                const factorPartido = nombreClub.nextElementSibling
+            
+                nombreClub.innerText = ""
+                factorPartido.innerText = ""
+                
+                const divCerrar = document.getElementById("cerrar")
+
+              
+                    
+                divCerrar.removeChild(divCerrar.childNodes[0,1]);
+                    
+                
+                intentos = 0
+                cartilla = false
+
+                
+            }
+            
 
 }
 
@@ -116,7 +201,70 @@ const actualizarGanancia = () =>{
     } )
 
 
+}
 
+
+
+const confirmarApuesta = () => {
+
+    const botonApostar = document.getElementById("apostar")
+
+
+    botonApostar.onclick = () =>{
+
+        const divInferior = document.getElementById("divinferior") 
+        
+
+        if(vacio == false){
+            vacio = true
+            const nuevoDiv = document.createElement("div")
+            nuevoDiv.setAttribute("id", "nuevodiv")
+    
+            const mensaje = document.createElement("text")
+            mensaje.innerText = "\n ¿Está Seguro? \n"
+    
+            const botonFinalSi = document.createElement("button")
+            botonFinalSi.setAttribute("id", "botonfinal")
+            botonFinalSi.innerText = "Sí"
+    
+            const botonFinalNo = document.createElement("button")
+            botonFinalNo.setAttribute("id", "botonfinal")
+            botonFinalNo.innerText = "No"
+    
+            botonFinalNo.onclick = () =>{
+    
+                nuevoDiv.remove()
+                vacio = false
+            }
+            
+            botonFinalSi.onclick = () =>{
+
+                const codigoFinal = document.getElementById("codigoPartidaFinal")
+                const seleccionFinal = document.getElementById("nombreseleccion").innerText
+                const montoFinal  = document.getElementById("monto").value
+                const gananciaFinal =  document.getElementById("total").value
+                
+                
+                
+
+        
+
+                console.log(codigoFinal.value,montoFinal,gananciaFinal,seleccionFinal)
+
+
+            }
+    
+            nuevoDiv.appendChild(mensaje)
+            nuevoDiv.appendChild(botonFinalSi)
+            nuevoDiv.appendChild(botonFinalNo)
+            divInferior.appendChild(nuevoDiv)
+
+        }
+
+
+
+
+    }
 
 }
 
