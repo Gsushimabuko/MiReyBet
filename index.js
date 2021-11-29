a43a2f1b01c1c58b915e8272d31a82ff3147c0a0
Jonathan Cardenas <greiff.jonathan@gmail.com>
2021-11-29 17:36:07 -0500

    ds

--- index.js
+++ index.js
@@ -642,11 +642,9 @@ app.get('/partida/eliminar/:codigo', async (req, res) => {
     res.redirect('/partida')
 })
 
-app.get('/banner/new', async (req, res) => {
+app.get('/banner/new', (req, res) => {
     if (req.session.username=="admin"){  
-        res.sendFile(__dirname + "/views/banner_new.ejs")
-        res.render('banner_new')
-      
+        res.sendFile(__dirname + "/views/banner_new.ejs", res.render('banner_new') )
     }
     else{
         res.redirect('/advertencia')
